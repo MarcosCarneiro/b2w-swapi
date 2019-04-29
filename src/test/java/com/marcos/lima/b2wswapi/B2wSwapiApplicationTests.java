d85576a02211e6e63594da6927507be0ba1b7fac
Marcos C Lima <marcos.lima.crn@gmail.com>
2019-04-29 10:46:39 -0300

    Add test classes in test suite

--- src/test/java/com/marcos/lima/b2wswapi/B2wSwapiApplicationTests.java
+++ src/test/java/com/marcos/lima/b2wswapi/B2wSwapiApplicationTests.java
@@ -2,18 +2,20 @@ package com.marcos.lima.b2wswapi;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
 import com.marcos.lima.b2wswapi.controllers.PlanetControllerTest;
+import com.marcos.lima.b2wswapi.repository.PlanetRepositoryTest;
 
-@RunWith(SpringRunner.class)
+@RunWith(Suite.class)
 @SpringBootTest
+@Suite.SuiteClasses({ PlanetControllerTest.class, PlanetRepositoryTest.class })
 public class B2wSwapiApplicationTests {
 
 	@Test
 	public void contextLoads() {
-		PlanetControllerTest planetControllerTest = new PlanetControllerTest();
+
 	}
 
 }
